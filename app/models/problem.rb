56c82953f54ec61dd0764c94d1dfa000c2b5b404
Stephen J Bamber <stephen.bamber20@googlemail.com>
2012-02-29 20:35:26 +0000

    removed some files

--- app/models/problem.rb
+++
@@ -1,9 +0,0 @@
-class Problem < ActiveRecord::Base
-  
-  belongs_to :user
-  has_many :solutions
-  has_and_belongs_to_many :categories
-  has_and_belongs_to_many :tags
-  has_and_belongs_to_many :error_messages
-  
-end
