ac0031cb144524a6d14f27259aa8d725567d7362
Stephen J Bamber <stephen.bamber20@googlemail.com>
2012-03-20 01:30:51 +0000

    Added basic client side form validation using client_side_validations gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,6 @@ class Category < ActiveRecord::Base
   has_many :posts, :through => :post_categories
   has_ancestry
   
-  validates :name, :presence => true, :length => { :within => 3..255 }, :uniqueness => true
+  validates :name, :presence => true, :length => { :within => 3..255 }#, :uniqueness => true
     
 end
