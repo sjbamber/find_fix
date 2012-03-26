80c4e3276ee8918b2a11b7cf340ed11c4ab5e96c
Stephen J Bamber <stephen.bamber20@googlemail.com>
2012-03-26 15:36:53 +0100

    Heroku fix

--- config/initializers/tanker.rb
+++ config/initializers/tanker.rb
@@ -1,1 +1,5 @@
-FindFix::Application.config.index_tank_url = 'http://:FnQdFRnOqQkaLm@ya1a.api.searchify.com'
+if ENV['RAILS_ENV'] === "production"
+  FindFix::Application.config.index_tank_url = 'http://:FnQdFRnOqQkaLm@ya1a.api.searchify.com'
+else
+  FindFix::Application.config.index_tank_url = 'http://:FnQdFRnOqQkaLm@ya1a.api.searchify.com'
+end
