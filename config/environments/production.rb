54bea0352af2c32a71a4cf00bcdaff65070f87d7
Stephen J Bamber <stephen.bamber20@googlemail.com>
2012-03-31 22:21:21 +0100

    Set new js and css for mobile dialog popups to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ FindFix::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( mobile/find-fix-mobile.min.css mobile/jquery.mobile.structure-1.0.1.min.css )
+  config.assets.precompile += %w( mobile/find-fix-mobile.min.css mobile/jquery.mobile.structure-1.0.1.min.css mobile/jquery.mobile.simpledialog.min.css mobile/jquery.mobile.simpledialog.min.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
