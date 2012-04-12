da0110ddbcda0d9b762722ec1569e6c7bf95aafe
Stephen J Bamber <stephen.bamber20@googlemail.com>
2012-04-12 23:39:11 +0100

    Fix to get smtp server to work in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,6 @@ FindFix::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
   config.action_mailer.default_url_options = { :host => "find-fix.com" }
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.raise_delivery_errors = true 
 end
