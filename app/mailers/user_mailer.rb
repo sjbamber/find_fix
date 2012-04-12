5ca1e8bae9d97af408f641f5be53d7599098cf60
Stephen J Bamber <stephen.bamber20@googlemail.com>
2012-04-12 23:30:59 +0100

    Fix to get smtp server to work in production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,6 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
+  default :from => "noreply@find-fix.com"
+  
   def password_reset(user)
     @user = user
 
