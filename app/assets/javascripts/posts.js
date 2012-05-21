e94b0dcbc73c240aaaa55ccc96a6ec621abb5ac7
Stephen J Bamber <stephen.bamber20@googlemail.com>
2012-05-21 10:59:43 +0100

    Minor fixes

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#post_submit').click(function() {
-        var data = $('#new_post').find('.nicEdit-main').html();
+        var data = $('#new_post').find('.nicEdit-main').html().text();
         $('#rich_textarea').html(data);
     });
 });
--- app/assets/javascripts/solutions.js
+++ app/assets/javascripts/solutions.js
@@ -1,7 +1,7 @@
 // Submit solution form by taking data from nicedit form and adding it to the textarea
 $(document).ready(function() {
     $('#solution_submit').click(function() {
-        var data = $('#new_solution').find('.nicEdit-main').html();
+        var data = $('#new_solution').find('.nicEdit-main').html().text();
         $('#rich_textarea').html(data);
     });
 });
